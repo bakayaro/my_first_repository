f66dc8923df91bf94aa179e12538ccdfe8ae3536
bakayaro <chrisbakayaro@gmail.com>
2015-08-31 11:18:06 -0700

    Update HelloWorld.java
    Adding employee class

--- HelloWorld.java
+++ HelloWorld.java
@@ -28,6 +28,21 @@ public class HelloWorld {
 		int number = randomGenerator.nextInt(1000);
 		System.out.println(number);
 		System.out.println("End of Program");
+		
+		/* Create two objects using constructor */
+	       	Employee empOne = new Employee("James Smith");
+	      	Employee empTwo = new Employee("Mary Anne");
+	
+	      	// Invoking methods for each object created
+	      	empOne.empAge(26);
+	      	empOne.empDesignation("Senior Software Engineer");
+	      	empOne.empSalary(1000);
+	      	empOne.printEmployee();
+	
+		empTwo.empAge(21);
+	      	empTwo.empDesignation("Software Engineer");
+	      	empTwo.empSalary(500);
+	      	empTwo.printEmployee();
 	}
 }
 // No newline at end of file
