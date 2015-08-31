c60af68e7111ff60d96ae8b3370510f51c03fcdc
bakayaro <chrisbakayaro@gmail.com>
2015-08-31 09:54:47 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -23,10 +23,11 @@ public class HelloWorld {
     public static void main(String[] args) {
 		Date date = new Date();
 		Random randomGenerator = new Random();
-        System.out.println("Hello, World");
+        	System.out.println("Hello, World");
 		System.out.println(date.toString());
 		int number = randomGenerator.nextInt(1000);
 		System.out.println(number);
+		System.out.println("End of Program");
 	}
 }
 // No newline at end of file
